7ca345cf44d4370f853a31ec056ae350dcf66807
Lucas <lucasoliveirars22@gmail.com>
2023-12-07 16:09:32 -0300

    add template

--- query/settings.py
+++ query/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'query.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
